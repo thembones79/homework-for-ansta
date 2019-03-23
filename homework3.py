4b66a77ed81d0e91f4feff1b610a66dbe9534532
thembones79 <michal.szumnarski@gmail.com>
2019-03-23 21:25:30 +0100

    did homework3

---
+++ homework3.py
@@ -0,0 +1,10 @@
+# ZADANIE 3. NALEŻY WYGENEROWAĆ LISTĘ LICZB OD 2 DO 5.5 ZE SKOKIEM CO 0.5, DANE WYNIKOWE MUSZĄ BYĆ TYPU DECIMAL.
+
+def generateList():
+    i = 2.0
+    while i <= 5.5:
+        print(i)
+        i += 0.5
+
+
+generateList()
